d953dd92b93d7cab124b04bb00e60e1f719fc2be
Vitalick37 <vit198837@mail.ru>
2020-04-08 16:03:37 +0300

    правки для хостинга

--- js/main.js
+++ js/main.js
@@ -91,4 +91,20 @@ $(document).ready(function () {
 
   $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+  // скролл
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href=#up]").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
 });
